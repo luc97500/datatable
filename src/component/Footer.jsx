354c60f7a66bec4c657b000d7939e31a10bc3155
lalit <luc97500@gmail.com>
2024-08-21 18:18:29 +0530

    Updated Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Footer} from "flowbite-react";
-
+import React from "react";
+import { Footer } from "flowbite-react";
 
 export function Footercomponent() {
   return (
     <div>
       <Footer container>
-      <Footer.Copyright href="#" by="Designed and Developed by Lalit Chaudhari" year={2022} />
-      <Footer.LinkGroup>
-        <Footer.Link href="#">About</Footer.Link>
-        <Footer.Link href="#">Privacy Policy</Footer.Link>
-        <Footer.Link href="#">Licensing</Footer.Link>
-        <Footer.Link href="#">Contact</Footer.Link>
-      </Footer.LinkGroup>
-    </Footer>
+        <Footer.Copyright
+          href="#"
+          by="Designed and Developed by Lalit Chaudhari"
+          year={2024}
+        />
+        <Footer.LinkGroup>
+          <Footer.Link
+            href="https://lalitchaudhariportfolio.netlify.app/"
+            target="_blank"
+          >
+            About
+          </Footer.Link>
+          <Footer.Link href="https://github.com/luc97500" target="_blank">
+            Github
+          </Footer.Link>
+          <Footer.Link
+            href="https://www.linkedin.com/in/lalit-chaudhari-129389216/"
+            target="_blank"
+          >
+            Linkedin
+          </Footer.Link>
+        </Footer.LinkGroup>
+      </Footer>
     </div>
-  )
+  );
 }
